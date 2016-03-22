fc1e5161eb61b5f9103008164a7c385b54393b02
dean thurston <deanthurston2791@gmail.com>
2016-03-22 00:38:11 -0500

    started on shader code
    added example shaders

---
+++ Assets/Scripts/ShaderScripts/Draw2DMetaballs.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+namespace UnityStandardAssets.ImageEffects
+{
+    [RequireComponent(typeof(Camera))]
+    public class Draw2DMetaballs : PostEffectsBase
+    {
+        public float radius;
+        public float testCenter;
+        // resources
+        public Shader shader;
+        public Color waterColor;
+        public List<Vector3> positions;
+        private Material metaballMaterial = null;
+        private Camera _camera;
+        private Texture2D positionsTexture;
+        new void Start()
+        {
+            CheckResources();
+            testCenter = Screen.width / 2;
+            if (_camera == null)
+                _camera = GetComponent<Camera>();
+        }
+
+        void OnEnable()
+        {
+
+            if (_camera == null)
+                _camera = GetComponent<Camera>();
+        }
+
+        void OnDisable()
+        {
+            
+            if (null != metaballMaterial)
+            {
+                DestroyImmediate(metaballMaterial);
+                metaballMaterial = null;
+            }
+        }
+        void CreatePositionsTexture()
+        {
+            positionsTexture = new Texture2D(positions.Count + 1, 1);
+            //Color[] positionsAsColors = new Color[positions.Count];
+            //for(int i = 0)
+            for (int i = 0; i < positions.Count; i++)
+            {
+                Vector3 pos = _camera.WorldToScreenPoint(positions[i]);
+                Color positionAsColor = new Color(pos.x, pos.y, pos.z);
+                positionsTexture.SetPixel(i + 1, 1, positionAsColor);
+            }
+            positionsTexture.Apply();
+        }
+        void OnRenderImage(RenderTexture source, RenderTexture destination)
+        {
+            CreatePositionsTexture();
+         
+            metaballMaterial.SetColor("_Color", waterColor);
+            metaballMaterial.SetFloat("_Radius", radius);
+            metaballMaterial.SetFloat("_TestX0", testCenter);
+            metaballMaterial.SetTexture("_PositionsTex", positionsTexture);
+            metaballMaterial.SetInt("_width", positionsTexture.width);
+
+
+            Graphics.Blit(source, destination, metaballMaterial);
+        }
+        public override bool CheckResources()
+        {
+            CheckSupport(true, true); // depth & hdr needed
+            metaballMaterial = CheckShaderAndCreateMaterial(shader, metaballMaterial);
+
+            return isSupported;
+        }
+
+
+    }
+}
---
+++ Assets/Shaders/2DMetaballs.shader
@@ -0,0 +1,87 @@
+﻿Shader "Hidden/2dMetaballs"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+		_PositionsTex("-", 2D) = "grey" {}
+		_Color("Water Color", Color) = (0.1,0.1 ,1,1)
+	}
+		CGINCLUDE
+	#include "UnityCG.cginc"
+	
+	sampler2D _PositionsTex;
+	float _Radius;
+	float4 _Color;
+	int _width;
+	float _TestX0;
+	float _TestY0;
+	float _TestX1;
+	float _TestY1;
+	
+	float GetIntensity(float2 start, float2 end, float radius)
+	{
+		return (radius / (((start.x - end.x) * (start.x - end.x)) + ((start.y - end.y) * (start.y - end.y)) + 0.000001));	
+	}
+
+	float totalIntensity()
+	{
+		for (int i = 0; i < _width; i++)
+		{
+			fixed4 col = tex2D(_PositionsTex, i);
+		}
+
+		return 0.0;
+	}
+	ENDCG
+
+	SubShader
+	{
+		// No culling or depth
+		Cull Off ZWrite Off ZTest Always
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = mul(UNITY_MATRIX_MVP, v.vertex);
+				o.uv = v.uv;
+				return o;
+			}
+			
+			sampler2D _MainTex;
+
+			fixed4 frag (v2f i) : COLOR
+			{
+				fixed4 col = tex2D(_MainTex, i.uv);
+				
+				float intensity = totalIntensity();
+				// just set color
+				
+				if(i.vertex.x > _TestX0)
+				col = _Color;
+
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
