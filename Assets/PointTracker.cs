c0f3d5f82deef172255f4021bd28e013e2ce59b9
dean thurston <deanthurston2791@gmail.com>
2016-09-30 17:22:24 -0500

    Added very simple points system
    get points from doing flips

---
+++ Assets/PointTracker.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointTracker : MonoBehaviour {
+
+    public int points;
+    float startingAngle;
+    public float changeInAngle;
+    public Rigidbody2D player;
+    // Use this for initialization
+	void Start ()
+    {
+        points = 0;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(Constants.player.onGround)
+        {
+            if (Mathf.Abs(changeInAngle) > 150)
+                points += (int)(Mathf.Abs(changeInAngle) / 2);
+            changeInAngle = 0;
+        }
+        else
+        {
+            float change = (startingAngle - player.transform.eulerAngles.z);
+            if(change < 150)
+                changeInAngle += change;
+
+        }
+
+        startingAngle = player.transform.eulerAngles.z;
+    }
+}
